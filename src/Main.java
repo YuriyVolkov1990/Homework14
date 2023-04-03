a2310bc758f7c3676669301ebe03f978e87e3c63
Professional <new99kpicem@gmail.com>
2023-04-04 00:42:07 +0300

    work

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
+    public static void changeValue (Integer value) {
+        //        value[0] = 99;
+//        value = new int[]{1, 2};
+//        System.out.println("Массив в методе");
+//        for (int i:
+//             value) {
+//            System.out.println(i+" ");
+//        }
+//        System.out.println();
+//        value = 22;
+//        System.out.println("value в методе = " + value);
+//        value = 22;
+//        System.out.println("value in changeValue = " + value);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Integer value = 33;
+//        System.out.println("value до метода = " + value);
+//        changeValue(value);
+//        System.out.println("value после метода = " + value);
+//        System.out.println("Массив до метода");
+//       int[] value = new int[]{3, 4};
+//        for (int i:
+//             value) {
+//           System.out.println(i+" ");
+//        }
+//        changeValue(value);
+//        System.out.println("Массив после метода");
+//        for (int i:
+//             value) {
+//           System.out.println(i + " ");
+//        }
+//        System.out.println("Массив до метода");
+//        for (int i:
+//             value) {
+//           System.out.println(i+" ");
+//        }
+//        System.out.println();
+//        changeValue(value);
+//        System.out.println("Массив после метода");
+//        for (int i:
+//                value) {
+//            System.out.println(i+" ");
+//        }
+//        System.out.println();
+//        int value = 33;
+//        System.out.println("value under changeValue = " + value);
+//        changeValue(value);
+//        System.out.println("value after changeValue = " + value);
     }
 }
